d5ab94fe3bae132b9ca3f984dd1eed0062d084e8
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-03-18 17:46:51 +0000

    Added data from api to hero section, added styling to hero section and responsivnes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -17,7 +17,10 @@ const Hero = () => {
   const [heroBg, setHeroBg] = useState("");
   const [eventName, setEventName] = useState("");
   const [eventCity, setEventCity] = useState("");
+  const [eventVenue, setEventVenue] = useState("");
   const [eventDate, setEventDate] = useState("");
+  const [eventTime, setEventTime] = useState("");
+  const [eventGenre, setEventGenre] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,8 +32,11 @@ const Hero = () => {
           const rndNumber = generateRandomNumber(data.length);
           setHeroBg(imageSizeApi(data[rndNumber].images, 1200));
           setEventName(data[rndNumber].name);
-          setEventCity(data[rndNumber].city);
-          setEventDate(data[rndNumber].date);
+          setEventCity(data[rndNumber]._embedded.venues[0].city.name);
+          setEventVenue(data[rndNumber]._embedded.venues[0].name);
+          setEventDate(data[rndNumber].dates.start.localDate);
+          setEventTime(data[rndNumber].dates.start.localTime);
+          setEventGenre(data[rndNumber].classifications[0].genre.name);
         }
       } catch (error) {
         console.error(error);
@@ -42,6 +48,10 @@ const Hero = () => {
     // Call fetchData when formCity changes
     fetchData();
   }, [formCity]); // Update when formCity changes
+
+  useEffect(() => {
+    console.log("Test:", events);
+  }, [events]); // Log when events state changes
 
   const heroBgUrl = {
     background: `url(${heroBg})`,
@@ -60,12 +70,46 @@ const Hero = () => {
         </div>
       )}
       <div className="hero" style={heroBgUrl}>
-        <div className="container-xxl position-relative">
-          <h1 className="text-white">Test</h1>
-          {/* Render event details */}
-          <p>{eventName}</p>
-          <p>{eventCity}</p>
-          <p>{eventDate}</p>
+        <div className="container-xxl position-relative hero-wraper ">
+          <div className="h-100 d-flex flex-column justify-content-center py-5">
+            <div className="hero-details-wraper">
+              <p className="grey m-0">{eventGenre}</p>
+              <h1 className="hero-title text-center text-sm-start">
+                {eventName.slice(0, 30)}
+              </h1>
+              <div className="event-details-wraper d-flex flex-column flex-sm-row col mt-5">
+                <div className="col-12 col-sm-6 col-md-5 col-xl-4 col-xxl-3 d-flex justify-content-center justify-content-sm-start mb-3 mb-sm-0">
+                  <div className="event-date d-flex flex-column justify-content-center align-items-center">
+                    <p className="text-center m-0">
+                      {new Date(eventDate).toLocaleString("default", {
+                        month: "short",
+                      })}
+                    </p>
+                    <p className="text-center m-0">
+                      {new Date(eventDate)
+                        .getDate()
+                        .toString()
+                        .padStart(2, "0")}
+                    </p>
+                  </div>
+                  <div className="event-date event-time d-flex flex-column justify-content-center align-items-center">
+                    <p className="text-center m-0">
+                      {eventTime.split(":").slice(0, 2).join(":")}
+                    </p>
+                  </div>
+                  <div className="event-city d-flex flex-column justify-content-center align-items-center">
+                    <p className="m-0">{eventVenue}</p>
+                  </div>
+                </div>
+                <div className="ms-3 more-info-wraper white col-12 col-sm-6 col-md-7 col-xl-8 col-xxl-9 start d-flex justify-content-center justify-content-sm-start">
+                  <div className="more-info d-flex flex-row align-items-center">
+                    <i className="fa-solid fa-play"></i>
+                    <p className="text m-0 ">More Details</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
