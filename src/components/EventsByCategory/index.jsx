7f4baf2a2d65e8b46fcd430a3fc0060953aa02ea
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-03-21 20:21:43 +0000

    Change search parameters by music type like Rock, jAzz etc

--- src/components/EventsByCategory/index.jsx
+++ src/components/EventsByCategory/index.jsx
@@ -1,30 +1,150 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./eventsbycategory.css";
+import { useFormCity } from "../../context/CityContext";
+import {
+  fetchEvents,
+  futureDayForApi,
+} from "../../eventsActions/eventsActions";
 
 const EventsByCategory = () => {
+  const { formCity } = useFormCity();
+  const [events, setEvents] = useState([]);
+  const [category, setCategory] = useState("jazz");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true); // Set isLoading to true when fetching data
+        const data = await fetchEvents(
+          undefined,
+          formCity,
+          undefined,
+          futureDayForApi(180),
+          category,
+          50
+        );
+        setEvents(data);
+        setIsLoading(false);
+        console.log("Button Events loaded:", data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false); // Set isLoading to false after data is fetched
+      }
+    };
+
+    // Call fetchData when formCity changes
+    fetchData();
+  }, [formCity, category]);
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+    console.log("Selected category:", event.target.value);
+  };
+
   return (
     <>
       <div className="container-xxl py-5">
         <h2 className="white mb-4">Choose a Category</h2>
         <div className="event-category-wraper d-flex flex-sm-row mb-5 flex-wrap flex-sm-no-wrap justify-content-center justify-content-sm-start">
-          <button className="event-category event-film d-flex flex-column justify-content-center align-items-center ">
+          <input
+            type="radio"
+            className="film-check d-none"
+            name="category"
+            id="film"
+            value="metal"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "metal"}
+          />
+          <label
+            className="event-category event-film d-flex flex-column justify-content-center align-items-center"
+            htmlFor="film"
+          >
             Film
-          </button>
-          <button className="event-category event-comedy d-flex flex-column justify-content-center align-items-center">
-            <span className="comedy-txt">Comedy</span>
-          </button>
-          <button className="event-category event-music d-flex flex-column justify-content-center align-items-center">
+          </label>
+          <input
+            type="radio"
+            className="comedy-check d-none"
+            name="category"
+            value="comedy"
+            id="comedy"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "comedy"}
+          />
+          <label
+            className="event-category event-comedy d-flex flex-column justify-content-center align-items-center"
+            htmlFor="comedy"
+          >
+            Comedy
+          </label>
+
+          <input
+            type="radio"
+            className="music-check d-none"
+            name="category"
+            value="music"
+            id="music"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "music"}
+          />
+          <label
+            className="event-category event-music d-flex flex-column justify-content-center align-items-center"
+            htmlFor="music"
+          >
             Music
-          </button>
-          <button className="event-category event-sports d-flex flex-column justify-content-center align-items-center">
-            <span className="sports-txt">Sports</span>
-          </button>
-          <button className="event-category category-theater d-flex flex-column justify-content-center align-items-center">
-            <span className="theater-txt">Theater</span>
-          </button>
-          <button className="event-category category-family d-flex flex-column justify-content-center align-items-center">
+          </label>
+          <input
+            type="radio"
+            className="sports-check d-none"
+            name="category"
+            value="sports"
+            id="sports"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "sports"}
+          />
+          <label
+            className="event-category event-sports d-flex flex-column justify-content-center align-items-center"
+            htmlFor="sports"
+          >
+            Sports
+          </label>
+          <input
+            type="radio"
+            className="theater-check d-none"
+            name="category"
+            value="theater"
+            id="theater"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "theater"}
+          />
+          <label
+            className="event-category category-theater d-flex flex-column justify-content-center align-items-center"
+            htmlFor="theater"
+          >
+            Theater
+          </label>
+          <input
+            type="radio"
+            className="family-check d-none"
+            name="category"
+            value="family"
+            id="family"
+            autoComplete="off"
+            onChange={handleCategoryChange}
+            checked={category === "family"}
+          />
+          <label
+            className="event-category category-family d-flex flex-column justify-content-center align-items-center"
+            htmlFor="family"
+          >
             Family
-          </button>
+          </label>
         </div>
         <div className="row cards-wraper row-cols-1 row-cols-lg-2 g-3">
           <div className="col">
--- src/eventsActions/eventsActions.jsx
+++ src/eventsActions/eventsActions.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Future date for Ticketmaster API (15 days ahead)
-export const futureDayForApi = () => {
+export const futureDayForApi = (futureDays) => {
   const currentDate = new Date();
   const futureDate = new Date(currentDate);
-  futureDate.setDate(currentDate.getDate() + 15);
+  futureDate.setDate(currentDate.getDate() + futureDays);
   return futureDate.toISOString().slice(0, -5) + "Z";
 };
 
@@ -27,12 +27,12 @@ export const fetchEvents = async (
   country = "GB",
   city = "London",
   startDate = startDateForApi(new Date()),
-  endDate = futureDayForApi(),
+  endDate = futureDayForApi(15),
   eventType = "Music",
   limit = 5
 ) => {
   const apiKey = "HjQcNIEkdwsQswwBQhfE1PO0smAoxyu4";
-  const apiUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apiKey}&countryCode=${country}&city=${city}&startDateTime=${startDate}&endDateTime=${endDate}&segmentName=${eventType}&size=${limit}`;
+  const apiUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=${apiKey}&countryCode=${country}&city=${city}&startDateTime=${startDate}&endDateTime=${endDate}&classificationName=${eventType}&size=${limit}`;
   // console.log("API URL:", apiUrl);
 
   try {
