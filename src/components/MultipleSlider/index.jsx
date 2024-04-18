01aa47b13689cddb803712cff675f1c0044aff82
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-04-18 11:41:20 +0100

    Added functionality to show placeholder for multislider while data is fetched or slider

--- src/components/MultipleSlider/index.jsx
+++ src/components/MultipleSlider/index.jsx
@@ -25,6 +25,9 @@ const CustomNextArrow = (props) => {
 
 const MultipleSlider = ({ popularEvents, title }) => {
   const [loading, setLoading] = useState(true);
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
 
   const settings = {
     dots: false,
@@ -70,96 +73,99 @@ const MultipleSlider = ({ popularEvents, title }) => {
 
   return (
     <>
-      {popularEvents && (
-        <div className="multislider-wraper postion-relative">
-          {loading && (
-            <>
-              <div className="container-xxl">
-                <div className="multislider-placeholder placeholder-glow  title mb-4">
-                  <span className="placeholder bg-white col-5"></span>
-                </div>
-              </div>
-              <div className="multislider-placeholder-card-wraper d-flex">
-                {Array.from({ length: 5 }, (_, index) => (
-                  <div
-                    className={`multislider-placeholder-card placeholder-${index} d-flex flex-column justify-content-between`}
-                    key={index}
-                  >
-                    <h5 className="multislider-placeholder-title placeholder-glow px-3 mt-3">
-                      <span className="placeholder bg-white col-12"></span>
-                    </h5>
-                    <div className="multislider-placeholder-genre-wraper">
-                      <p className="card-text placeholder-glow d-flex align-items-center h-100 ps-3">
-                        <span className="placeholder bg-white col-6"></span>
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-          {title && (
+      <div className="multislider-wraper postion-relative">
+        {loading && (
+          <>
             <div className="container-xxl">
-              <h2 className="multislider-title mb-4">{title}</h2>
+              <div className="multislider-placeholder placeholder-glow  title mb-4">
+                <span className="placeholder bg-white col-5"></span>
+              </div>
             </div>
-          )}
-          <Slider
-            {...settings}
-            className=" multislider text-white position-relative "
-          >
-            {popularEvents.slice(0, 10).map((event) => (
-              <div
-                className="card-event-wraper overflow-hidden "
-                key={event.id}
-              >
-                <div className="img-card-wraper position-relative ">
-                  <div className="overlay"></div>
-                  <img
-                    src={imageSizeApi(event?.images, 350)}
-                    alt={`Artist - ${event?.name} image`}
-                    className="card-event-img"
-                  />
-                  <h5 className="event-card-title px-2">{event?.name}</h5>
-                  <div className="event-card-genre-wraper d-flex justify-content-between">
-                    <p className="event-card-genre m-0">
-                      {event?.classifications?.[0].genre?.name}
+            <div className="multislider-placeholder-card-wraper d-flex">
+              {Array.from({ length: 5 }, (_, index) => (
+                <div
+                  className={`multislider-placeholder-card placeholder-${index} d-flex flex-column justify-content-between`}
+                  key={index}
+                >
+                  <h5 className="multislider-placeholder-title placeholder-glow px-3 mt-3">
+                    <span className="placeholder bg-white col-12"></span>
+                  </h5>
+                  <div className="multislider-placeholder-genre-wraper">
+                    <p className="card-text placeholder-glow d-flex align-items-center h-100 ps-3">
+                      <span className="placeholder bg-white col-6"></span>
                     </p>
                   </div>
-                  <div className="event-card-details-wraper d-flex justify-content-between">
-                    <div className="event-date-wraper d-flex flex-column justify-content-center align-items-center">
-                      <p className="m-0">
-                        {new Date(event?.dates?.start?.dateTime).toLocaleString(
-                          "default",
-                          {
-                            month: "short",
-                          }
-                        )}
-                      </p>
-                      <p className="m-0">
-                        {new Date(event?.dates?.start?.dateTime)
-                          .getDate()
-                          .toString()
-                          .padStart(2, "0")}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+
+        {popularEvents && (
+          <>
+            {title && (
+              <div className="container-xxl">
+                <h2 className="multislider-title mb-4">{title}</h2>
+              </div>
+            )}
+            <Slider
+              {...settings}
+              className=" multislider text-white position-relative "
+            >
+              {popularEvents.slice(0, 10).map((event) => (
+                <div
+                  className="card-event-wraper overflow-hidden "
+                  key={event.id}
+                >
+                  <div className="img-card-wraper position-relative ">
+                    <div className="overlay"></div>
+                    <img
+                      src={imageSizeApi(event?.images, 350)}
+                      alt={`Artist - ${event?.name} image`}
+                      className="card-event-img"
+                      onLoad={handleImageLoad}
+                    />
+                    <h5 className="event-card-title px-2">{event?.name}</h5>
+                    <div className="event-card-genre-wraper d-flex justify-content-between">
+                      <p className="event-card-genre m-0">
+                        {event?.classifications?.[0].genre?.name}
                       </p>
                     </div>
-                    <div className="event-card-time-venue-wraper d-flex flex-column justify-content-between">
-                      <p className="event-card-venue m-0">
-                        {event?._embedded?.venues?.[0].name}
-                      </p>
-                      <p className="event-card-time text-end m-0">
-                        {event.dates.start.localTime
-                          .split(":")
-                          .slice(0, 2)
-                          .join(":")}
-                      </p>
+                    <div className="event-card-details-wraper d-flex justify-content-between">
+                      <div className="event-date-wraper d-flex flex-column justify-content-center align-items-center">
+                        <p className="m-0">
+                          {new Date(
+                            event?.dates?.start?.dateTime
+                          ).toLocaleString("default", {
+                            month: "short",
+                          })}
+                        </p>
+                        <p className="m-0">
+                          {new Date(event?.dates?.start?.dateTime)
+                            .getDate()
+                            .toString()
+                            .padStart(2, "0")}
+                        </p>
+                      </div>
+                      <div className="event-card-time-venue-wraper d-flex flex-column justify-content-between">
+                        <p className="event-card-venue m-0">
+                          {event?._embedded?.venues?.[0].name}
+                        </p>
+                        <p className="event-card-time text-end m-0">
+                          {event.dates.start.localTime
+                            .split(":")
+                            .slice(0, 2)
+                            .join(":")}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </Slider>
-        </div>
-      )}
+              ))}
+            </Slider>
+          </>
+        )}
+      </div>
     </>
   );
 };
