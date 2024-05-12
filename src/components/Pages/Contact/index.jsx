5d7f7a1a696fe073250f943ccae27f20cd79f975
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-05-12 19:37:12 +0100

    Form is working, trying to change default submision page

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./contact.css";
 
 const Contact = () => {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
-
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById("contact-form");
+    let formData = new FormData(myForm);
+    fetch("/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => navigate("/search"))
+      .catch((error) => alert(error));
+  };
   return (
     <div id="contact-page">
       <div className="contact-test"></div>
-      <form name="contact" method="POST" className="mt-5">
+      <form
+        name="contact"
+        method="POST"
+        className="mt-5"
+        onSubmit={handleSubmit}
+      >
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
