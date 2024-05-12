51492713e5cfc2f5b283a3e8b818a9b9901f5fc3
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-05-12 19:23:25 +0100

    Still testing forms

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -3,50 +3,19 @@ import "./contact.css";
 
 const Contact = () => {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("https://ukevents.netlify.app/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          "form-name": "contact",
-          name,
-          email,
-          message,
-        }).toString(),
-      });
-      if (response.ok) {
-        console.log("Form submitted successfully!");
-      } else {
-        console.error("Form submission failed:", response);
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  };
 
   return (
     <div id="contact-page">
       <div className="contact-test"></div>
-      <form
-        onSubmit={handleSubmit}
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        className="mt-5"
-      >
+      <form name="contact" method="POST" className="mt-5">
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
             Name:
             <input
               type="text"
+              id="name"
               name="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
@@ -55,20 +24,10 @@ const Contact = () => {
         </div>
         <div>
           <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
             Message:
             <textarea
               name="message"
+              id="message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
