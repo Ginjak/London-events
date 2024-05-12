458143d5308f9a8edf424d3060d7039a3d21a34a
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-05-12 20:05:57 +0100

    test

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -7,10 +7,36 @@ const Contact = () => {
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Form submission via Netlify
+    try {
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({
+          "form-name": "contact",
+          name,
+          message,
+        }).toString(),
+      });
+      // Redirect to "/search" page upon successful submission
+      navigate("/search");
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  };
+
   return (
     <div id="contact-page">
       <div className="contact-test"></div>
-      <form name="contact" method="POST" className="mt-5">
+      <form
+        name="contact"
+        method="POST"
+        className="mt-5"
+        onSubmit={handleSubmit}
+      >
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
