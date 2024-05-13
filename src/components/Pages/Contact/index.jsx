681d81b0b1084d10eb0daad93de3015a8181f871
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-05-13 15:24:58 +0100

    Added some styling to contact form

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./contact.css";
 
 const Contact = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState(true);
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    let interval;
+    if (submitted) {
+      interval = setInterval(() => {
+        setCountdown((count) => count - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [submitted]);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      setSubmitted(false);
+      setCountdown(5);
+    }
+  }, [countdown]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       await fetch("/", {
         method: "POST",
@@ -28,47 +46,72 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact-page">
-      <div className="contact-test"></div>
-      {submitted ? ( // If form is submitted, display thank you message
+    <div
+      id="contact-page"
+      className="d-flex justify-content-center align-items-center"
+    >
+      {submitted ? (
         <div className="thank-you-message">
           <h2>Thank you for your message!</h2>
           <p>We'll get back to you as soon as possible.</p>
+          <p>Returning to form in {countdown} seconds...</p>
         </div>
       ) : (
-        // If form is not submitted, display the form
-        <form
-          name="contact"
-          method="POST"
-          className="mt-5"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div>
-            <label>
-              Name:
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Message:
-              <textarea
-                name="message"
-                id="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </label>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <div className="contact-form-wraper">
+          <h2>Contact us</h2>
+          <h5>
+            Got any questions or suggestions? Fill out this form to reach out
+          </h5>
+          <form
+            name="contact"
+            method="POST"
+            className="mt-5"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="name-email-wraper d-flex gap-2">
+              <div className="w-100">
+                <label className="w-100">
+                  <input
+                    className="form-input"
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Enter your name"
+                  />
+                </label>
+              </div>
+              <div className="w-100">
+                <label className="w-100">
+                  <input
+                    className="form-input"
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter your email"
+                  />
+                </label>
+              </div>
+            </div>
+            <div>
+              <label className="w-100 mt-2">
+                <textarea
+                  className="w-100 form-input"
+                  name="message"
+                  id="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  placeholder="Enter your mesage"
+                />
+              </label>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       )}
     </div>
   );
