032330a4d6519dea67708b023d28143916e27b53
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-05-13 14:29:57 +0100

    Form test

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./contact.css";
 
 const Contact = () => {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
-  const navigate = useNavigate();
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Form submission via Netlify
+
     try {
       await fetch("/", {
         method: "POST",
@@ -20,8 +19,8 @@ const Contact = () => {
           message,
         }).toString(),
       });
-      // Redirect to "/search" page upon successful submission
-      navigate("/search");
+
+      setSubmitted(true); // Set submitted to true after successful submission
     } catch (error) {
       console.error("Error submitting form:", error);
       alert("An error occurred. Please try again later.");
@@ -31,38 +30,46 @@ const Contact = () => {
   return (
     <div id="contact-page">
       <div className="contact-test"></div>
-      <form
-        name="contact"
-        method="POST"
-        className="mt-5"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label>
-            Name:
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
+      {submitted ? ( // If form is submitted, display thank you message
+        <div className="thank-you-message">
+          <h2>Thank you for your message!</h2>
+          <p>We'll get back to you as soon as possible.</p>
         </div>
-        <div>
-          <label>
-            Message:
-            <textarea
-              name="message"
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      ) : (
+        // If form is not submitted, display the form
+        <form
+          name="contact"
+          method="POST"
+          className="mt-5"
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div>
+            <label>
+              Name:
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Message:
+              <textarea
+                name="message"
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
---
+++ src/components/Pages/FormSubmission/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./formsubmission.css";
+
+const FormSubmission = () => {
+  return (
+    <div id="form-submission">
+      <p>Thank you for</p>
+    </div>
+  );
+};
+import "./formsubmission.css";
+
+export default FormSubmission;
